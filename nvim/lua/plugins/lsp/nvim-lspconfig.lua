1f775f5aeca2e42522ba82b8e4593307934bb71b
Jesus-Pablo Alfaro <alfaro.pablo13@gmail.com>
2024-10-22 14:26:38 -0700

    Added keybind for jump declaration

--- nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -9,12 +9,27 @@ return {
     local mason_lspconfig = require("mason-lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    -- Define the on_attach function to map keys after the LSP server attaches to the buffer
+    local on_attach = function(client, bufnr)
+      local opts = { noremap = true, silent = true }
+
+      -- Bind `gd` to jump to declaration
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+
+      -- Optionally, you can also bind keys for other LSP features
+      -- Example: bind `gD` for go to definition
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    end
+
     mason_lspconfig.setup_handlers({
       function(server_name)
         lspconfig[server_name].setup({
           capabilities = capabilities,
+          on_attach = on_attach,  -- Add the on_attach function here
         })
       end,
     })
   end,
 }
+
