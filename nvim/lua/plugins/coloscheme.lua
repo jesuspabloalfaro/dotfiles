f8d4460790d17e8db807970a75f664ff59844c27
Jesus-Pablo Alfaro <alfaro.pablo13@gmail.com>
2024-10-22 09:22:18 -0700

    Added new items

--- nvim/lua/plugins/coloscheme.lua
+++ nvim/lua/plugins/coloscheme.lua
@@ -2,6 +2,10 @@ return {
     "rebelot/kanagawa.nvim",
     priority = 1000,
     config = function()
+        require("kanagawa").setup({
+            transparent = true,
+            theme = "dragon"
+        })
         vim.cmd([[colorscheme kanagawa]])
     end,
 }
